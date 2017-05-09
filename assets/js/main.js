0d58425dbea3e7b884d7dc46dcaa81809c114852
caroquintana <caro.quintana.n@gmail.com>
2017-05-09 12:48:59 -0400

    añadiendo titulos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,6 @@
-alert("hola mundo");
+//Añadiendo un título
+var elemento = document.createElement("h2");
+var contenido = document.createTextNode("STUDENT RECORDS");
+elemento.appendChild(contenido);
+elemento.setAttribute("align", "center");
+document.getElementById("titulo").appendChild(elemento);
