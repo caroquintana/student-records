c17846004564c863b52cc0a96bfe04824b7fb756
Anayn Pavez <nanyn.pavez@gmail.com>
2017-05-09 13:07:24 -0300

    versión con constructor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-alert("hola mundo");
+function Estudiante{
+	this.nombre= nombre;
+	this.tech= tech;
+	this.skills= skills;
+	this.status= status;
+}
+
+
+
+function imprimir(){
+	var contenedor = document.getElementById('contenedorEstudiante');
+	contenedor.innerHTML;
+}
+
+
+function actualizacion(){}
+function empleabilidad(){}
