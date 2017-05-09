0772650a8868ce425df55fa8761dcd9f05d97902
maitc <maibeet.torres@gmail.com>
2017-05-09 13:01:50 -0400

    prompts ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,5 @@
-alert("MUERTE");
+function agregarEstudiante(){
+	var nombre = prompt("Ingrese Nombre de estudiante");
+	var porcentajeTecnico = prompt("Ingrese Porcentaje Técnico");
+	var porcentajeHSE = prompt("Ingrese Porcentaje Habilidades Socio-Emocionales");
+}
